5d83b9e50c31d4f84d6e82b788386c4457d9d81e
Alan Yahir Fuentes Baez <fuentesyahir060205@gmail.com>
2022-07-24 20:13:21 -0500

    programa que calcula raiz cuadrada y la potencia

--- python.py
+++ python.py
@@ -4,7 +4,11 @@ num=1
 
 while(num!=0):
     print('Programa que calcula la raíz cuadrada')
+    print("progama que calcula la potencia cuadrada")
+    
     num = float(input('Ingresa el número que quieres calcular:'))
     print('La raiz cuadrada es:')
     print(math.sqrt(num))
+    print('La potencia cuadrada es:')
+    print(pow(num,2))
 
