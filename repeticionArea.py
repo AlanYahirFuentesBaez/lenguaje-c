09d03578ae00fd31b3c8dd6f5e7b4ad62683126c
Alan Yahir Fuentes Baez <fuentesyahir060205@gmail.com>
2022-08-20 16:08:55 -0500

    tabla de multiplicar

--- repeticionArea.py
+++ repeticionArea.py
@@ -4,12 +4,12 @@ def area_triangulo(b,h):
 
 def tablaMultiplicar(tabla):
     multiplicador=0
-    while(multiplicador<10):        
+    while(multiplicador<9):        
         multiplicador=multiplicador+1
         resultado=tabla*multiplicador
         print("",tabla,"*",multiplicador,"resultado es: ",resultado)
         
-numero=0
+numero=9
 while(numero<10):
     numero=numero+1
     print(tablaMultiplicar(numero))
