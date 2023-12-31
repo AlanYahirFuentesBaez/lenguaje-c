0cd20d96d429a6e08b4dd4811ac5a596dcea8c1c
Alan Yahir Fuentes Baez <fuentesyahir060205@gmail.com>
2023-12-30 21:52:02 -0600

    comentar el programa

--- hola.java
+++ hola.java
@@ -1,30 +1,52 @@
 /*Librería para poder ingresar datos */
 import java.util.Scanner;
-
+ /**
+esta la clase principal
+  */
 public class hola {
 
     /**
      * @param args
      */
-    public static void main(String[] args) {
 
+    /**
+     este es el cuerpo principal
+     */
+     public static void main(String[] args) {
+    /**
+    instancia para poder ingresar datos mediante la terminal
+     */
         Scanner scanner = new Scanner(System.in);
-
+    /**
+    declaracion de las variables
+     */
         int x,y,z;
+    /**
+    mensaje al usuario
+     */
         System.out.println("Este es un programa que suma 2 números cualesquiera");
-
+    /**
+    es el numero que se va a introducir con la letra
+     */
         System.out.println("Ingresa un número x:");
         x=scanner.nextInt();
 
         System.out.println("Ingresa un número y:");
         y=scanner.nextInt();
-
+    /**
+    es el resultado de la operacion del programa 
+     */
 
         z=x-y;
+    /**
+    imprime el resultado de el programa
+     */
         System.out.println("El resultado es:");
         System.out.println(z);     
-        
-
+    /**
+    fin de el programa 
+    */  
+   
         
         
     }
